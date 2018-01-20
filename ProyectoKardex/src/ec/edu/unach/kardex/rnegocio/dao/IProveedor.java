07b4e282f9593686a689733df6b346693fa74171
ANDRES OROZCO <JhonPC@DESKTOP-T9J7GH5>
2018-01-20 10:43:11 -0500

    correccion errores Interfaz

--- ProyectoKardex/src/ec/edu/unach/kardex/rnegocio/dao/IProveedor.java
+++ ProyectoKardex/src/ec/edu/unach/kardex/rnegocio/dao/IProveedor.java
@@ -3,9 +3,9 @@ package ec.edu.unach.kardex.rnegocio.dao;
 import ec.edu.unach.kardex.rnegocio.entidades.*;
 import java.util.*;
 public interface IProveedor {
-    public int ingresar(Proveedor detalleVenta) throws Exception;
-    public int modificar(Proveedor detalleVenta) throws Exception;
-    public int eliminar(Proveedor detalleVenta) throws Exception;
+    public int ingresar(Proveedor proveedor) throws Exception;
+    public int modificar(Proveedor proveedor) throws Exception;
+    public int eliminar(Proveedor proveedor) throws Exception;
     public Proveedor obtener(String ruc) throws Exception;
     public ArrayList<Proveedor> obtener() throws Exception;
 }
