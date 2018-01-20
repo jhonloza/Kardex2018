e10e7cf2a5002c6e2102021156dd911a70bda561
Wen <Wen@DESKTOP-SPILHLE>
2018-01-20 01:45:36 -0500

    CAMBIO

--- ProyectoKardex/src/ec/edu/unach/kardex/rnegocio/entidades/Producto.java
+++ ProyectoKardex/src/ec/edu/unach/kardex/rnegocio/entidades/Producto.java
@@ -1,17 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.unach.kardex.rnegocio.entidades;
 
-/**
- *
- * @author DELL
- */
 public class Producto {
+
+    
+    private int codProducto;
+    private Categoria categoria;
+    private int nombre;
+    private Double precio;
 
     public Producto() {
     }
-    
+      public Producto(int codProducto, Categoria categoria, int nombre, Double precio) {
+        this.codProducto = codProducto;
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getCodProducto() {
+        return codProducto;
+    }
+
+    public void setCodProducto(int codProducto) {
+        this.codProducto = codProducto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+      
 }
+
