4d1b4194ca85c2a8f31af891b74a34ec47d075c8
Jhon <DELL@DESKTOP-PFTNPU5>
2018-01-20 00:01:05 -0500

    entidades

--- ProyectoKardex/src/ec/edu/unach/kardex/rnegocio/entidades/DetalleVenta.java
+++ ProyectoKardex/src/ec/edu/unach/kardex/rnegocio/entidades/DetalleVenta.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.unach.kardex.rnegocio.entidades;
 
-/**
- *
- * @author DELL
- */
 public class DetalleVenta {
+    private int codDetalleVenta;
+    private int producto;
+    private FacturaVenta facturaVenta;
+    private int cantidad;
+    private double precioTotal;
+
+    public DetalleVenta() {
+    }
+
+    public DetalleVenta(int codDetalleVenta, int producto, FacturaVenta facturaVenta, int cantidad, double precioTotal) {
+        this.codDetalleVenta = codDetalleVenta;
+        this.producto = producto;
+        this.facturaVenta = facturaVenta;
+        this.cantidad = cantidad;
+        this.precioTotal = precioTotal;
+    }
+
+    public int getCodDetalleVenta() {
+        return codDetalleVenta;
+    }
+
+    public void setCodDetalleVenta(int codDetalleVenta) {
+        this.codDetalleVenta = codDetalleVenta;
+    }
+
+    public int getProducto() {
+        return producto;
+    }
+
+    public void setProducto(int producto) {
+        this.producto = producto;
+    }
+
+    public FacturaVenta getFacturaVenta() {
+        return facturaVenta;
+    }
+
+    public void setFacturaVenta(FacturaVenta facturaVenta) {
+        this.facturaVenta = facturaVenta;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public void setPrecioTotal(double precioTotal) {
+        this.precioTotal = precioTotal;
+    }
+    
     
 }
