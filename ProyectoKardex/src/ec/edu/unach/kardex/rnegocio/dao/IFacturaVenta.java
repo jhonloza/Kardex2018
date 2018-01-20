174c615b7e88eca22a8f19c953408476eee0e1c7
ANDRES OROZCO <JhonPC@DESKTOP-T9J7GH5>
2018-01-20 01:45:29 -0500

    actualizacion factura ventas

--- ProyectoKardex/src/ec/edu/unach/kardex/rnegocio/dao/IFacturaVenta.java
+++ ProyectoKardex/src/ec/edu/unach/kardex/rnegocio/dao/IFacturaVenta.java
@@ -7,9 +7,9 @@ import java.util.*;
 
 public interface IFacturaVenta {
     
-     public int insertar (FacturaVenta etiqueta) throws Exception;
-    public int modificar (FacturaVenta etiqueta) throws Exception; 
-    public int eliminar (FacturaVenta etiqueta) throws Exception;
+     public int insertar (FacturaVenta facturaventa) throws Exception;
+    public int modificar (FacturaVenta facturaventa) throws Exception; 
+    public int eliminar (FacturaVenta facturaventa) throws Exception;
     public FacturaVenta obtener (int codFacturaVenta ) throws Exception;
     public ArrayList<FacturaVenta> obtener() throws Exception;
     
