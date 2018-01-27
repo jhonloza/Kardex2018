7bda3506a61cacb06eaa1bc2033545b0728b9ee5
DELL <DELL@DESKTOP-PFTNPU5>
2018-01-26 23:54:39 -0500

    actualizacion test facutra compra

--- ProyectoKardex/test/ec/edu/unach/kardex/TestFacturaCompra.java
+++ ProyectoKardex/test/ec/edu/unach/kardex/TestFacturaCompra.java
@@ -1,45 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.unach.kardex;
 
+import ec.edu.unach.kardex.rnegocio.dao.*;
+import ec.edu.unach.kardex.rnegocio.entidades.*;
+import ec.edu.unach.kardex.rnegocio.impl.*;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.*;
+import java.sql.*;
 
-/**
- *
- * @author DELL
- */
+
 public class TestFacturaCompra {
-    
+
     public TestFacturaCompra() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+
+    public void testGeneral() {
+
+        IFacturaCompra FacComDao = new ImplFacturaCompra();
+
+        // TEST INSERETAR 
+        int filas = 0;
+
+        Proveedor pro = new Proveedor("", "", "", "", "");
+        FacturaCompra fc = new FacturaCompra(1, new java.util.Date(), pro);
+
+        try {
+            filas = FacComDao.insertar(fc);
+            System.out.println("Filas insertadas: " + filas);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        assertEquals(filas > 0,true);
+        
+        /// listar por codigo test
+        
+        FacturaCompra fa = new FacturaCompra();
+        
+        try {
+            fa = FacComDao.obtener(1);
+            System.out.println(fa.getCodFacturaCompra()+""+fa.getFecha()+""+fa.getProveedor().getRuc());
+                    
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+        
+        assertEquals(fa != null, true );
+        //////test listado
+        
+        ArrayList<FacturaCompra> fac = new ArrayList<>();
+        try {
+            fac = FacComDao.obtener();
+            for (FacturaCompra far : fac ){
+                 System.out.println(fa.getCodFacturaCompra()+""+fa.getFecha()+""+fa.getProveedor().getRuc());
+            }
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+            
+        }
+        
+        
+
+    }
 }
