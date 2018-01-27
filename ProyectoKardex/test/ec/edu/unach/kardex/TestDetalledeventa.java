17905b56eda6c286b7bfd6ac59579195b939073b
stian <stian@WINDOWS-I0KJ897>
2018-01-27 02:15:22 -0500

    correcciones sql

--- ProyectoKardex/test/ec/edu/unach/kardex/TestDetalledeventa.java
+++ ProyectoKardex/test/ec/edu/unach/kardex/TestDetalledeventa.java
@@ -1,14 +1,35 @@
 
-package ec.edu.unach.kardex;
+//package ec.edu.unach.kardex;
+//
+//import ec.edu.unach.kardex.rnegocio.dao.ICategoria;
+//import ec.edu.unach.kardex.rnegocio.dao.IDetalleVenta;
+//import ec.edu.unach.kardex.rnegocio.entidades.Categoria;
+//import ec.edu.unach.kardex.rnegocio.impl.ImplCategoria;
+//import ec.edu.unach.kardex.rnegocio.impl.ImplDetalleVenta;
+//import java.util.ArrayList;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//
+//public class TestDetalledeventa {
+//   @Test
+//    public void testGeneral() {
+//        IDetalleVenta deveDao = new ImplDetalleVenta();
+        ///////////INSERTAR TEST
+//        int filas = 0;
+//
+//         Producto  producto = new Producto(1, "Bryan", new java.util.Date(), new java.util.Date());
+//         FacturaVenta facturaventa = new FacturaVenta(1, "Michu", new java.util.Date(), new java.util.Date());
+//        DetalleVenta detalleventa = new DetalleVenta(1,20,3);
+//       
+//
+//        try {
+//            filas = deveDao.insertar(per);
+//            System.out.println("Filas insertadas: " + filas);
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
+//        assertEquals(filas > 0, true);  
 
-import ec.edu.unach.kardex.rnegocio.dao.ICategoria;
-import ec.edu.unach.kardex.rnegocio.entidades.Categoria;
-import ec.edu.unach.kardex.rnegocio.impl.ImplCategoria;
-import java.util.ArrayList;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class TestDetalledeventa {
-    
-    
-}
+//    }
+//}
+//    
