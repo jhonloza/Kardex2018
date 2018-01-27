9cd808dec3b26a0a3ceb7e1b10f2dc4f1c72291b
stian <stian@WINDOWS-I0KJ897>
2018-01-26 22:53:14 -0500

    correcciones sql

---
+++ ProyectoKardex/test/ec/edu/unach/kardex/TestDetalledeventa.java
@@ -0,0 +1,14 @@
+
+package ec.edu.unach.kardex;
+
+import ec.edu.unach.kardex.rnegocio.dao.ICategoria;
+import ec.edu.unach.kardex.rnegocio.entidades.Categoria;
+import ec.edu.unach.kardex.rnegocio.impl.ImplCategoria;
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestDetalledeventa {
+    
+    
+}
